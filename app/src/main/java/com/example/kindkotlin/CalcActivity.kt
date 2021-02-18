7699f829eca2f2b09e2e08855c223746a0c8b66e
Neo <speldipn@gmail.com>
2021-02-19 00:12:41 +0900

    fix import

--- app/src/main/java/com/example/kindkotlin/CalcActivity.kt
+++ app/src/main/java/com/example/kindkotlin/CalcActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_calc.*
 import java.util.*
 
 class CalcActivity: AppCompatActivity() {
