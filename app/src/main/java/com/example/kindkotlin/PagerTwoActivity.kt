33df7f57a95223a26c1382fb194bda1b70219974
Neo <speldipn@gmail.com>
2021-02-22 11:37:41 +0900

    Page circle indicator

--- app/src/main/java/com/example/kindkotlin/PagerTwoActivity.kt
+++ app/src/main/java/com/example/kindkotlin/PagerTwoActivity.kt
@@ -5,10 +5,12 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_pager.view.*
 import kotlinx.android.synthetic.main.activity_pager_two.*
+import kotlinx.android.synthetic.main.activity_pager_two.view.*
+import me.relex.circleindicator.CircleIndicator2
 
 class PagerTwoActivity : FragmentActivity() {
 
@@ -19,28 +21,32 @@ class PagerTwoActivity : FragmentActivity() {
     }
 
     private fun setup() {
-        viewPager.adapter = CustomPagerAdapter(this)
-        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-//        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
-        viewPager.offscreenPageLimit = 3
+        pager.adapter = CustomPagerAdapter(this)
+        pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+//        pager.orientation = ViewPager2.ORIENTATION_VERTICAL
+        pager.offscreenPageLimit = 3
 
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+        pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
+                indicator.animatePageSelected(position)
                 showToast("${position + 1} selected!")
             }
         })
 
         val clickListener = View.OnClickListener { v ->
             when(v.id) {
-                R.id.oneButton -> viewPager.currentItem = 0
-                R.id.twoButton -> viewPager.currentItem = 1
-                R.id.threeButton -> viewPager.currentItem = 2
+                R.id.oneButton -> pager.currentItem = 0
+                R.id.twoButton -> pager.currentItem = 1
+                R.id.threeButton -> pager.currentItem = 2
             }
         }
         oneButton.setOnClickListener(clickListener)
         twoButton.setOnClickListener(clickListener)
         threeButton.setOnClickListener(clickListener)
+
+        indicator.setViewPager(pager)
+        indicator.createIndicators(3,0)
     }
 
     inner class CustomPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
